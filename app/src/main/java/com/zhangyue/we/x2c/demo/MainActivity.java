48eac660318a98061e22a66f94da9033701a9cfa
chengwei <chengwei@zhangyue.com>
2018-08-30 19:57:52 +0800

    Description :support ButterKnife

--- app/src/main/java/com/zhangyue/we/x2c/demo/MainActivity.java
+++ app/src/main/java/com/zhangyue/we/x2c/demo/MainActivity.java
@@ -3,26 +3,32 @@ package com.zhangyue.we.x2c.demo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
+import com.zhangyue.we.x2c.X2C;
+import com.zhangyue.we.x2c.ano.Xml;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
+@Xml(layouts = R.layout.activity)
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity);
-
-
+        X2C.setContentView(this, R.layout.activity);
+        ButterKnife.bind(this);
     }
 
-    public void startX2C(View v) {
+
+    @OnClick(R.id.x2c)
+    void toX2C() {
         startActivity(new Intent(this, ActivityX2C.class));
     }
 
-    public void startXML(View v) {
+    @OnClick(R.id.xml)
+    void toXML() {
         startActivity(new Intent(this, ActivityXML.class));
     }
-
-
 }
